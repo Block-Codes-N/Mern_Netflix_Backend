41c150ff7a1e8e4a1e5f99c67666773492b6ddde
Noman Shah <139675460+Block-Codes-N@users.noreply.github.com>
2024-01-18 08:16:56 +0500

    before router dom

--- routes/Auth.js
+++ routes/Auth.js
@@ -3,6 +3,8 @@ const router = require('express').Router();
 
 // Importing the 'User' model from the '../models/User' file
 const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+
 
 // Importing the 'CryptoJS' library for encrypting and decrypting data
 const CryptoJS = require("crypto-js");
@@ -42,11 +44,14 @@ router.post('/login', async(req, res) => {
 
         // Comparing the decrypted password with the password from the request body
         originalPassword !== req.body.password && res.status(401).json("Wrong Password or Username!")
-        
+
+        const accessToken = jwt.sign(
+            {id: user._id, isAdmin: user.isAdmin}, process.env.SECRET_KEY,{expiresIn: "5d"}
+        ); 
         // Extracting password from user data and creating a new object without the password
         const {password, ...info} = user._doc; 
         // Sending a successful response with user data (excluding password)
-        res.status(201).json(info);
+        res.status(200).json({ ...info, accessToken});
     } catch (err) {
         // Handling any errors that occur during the login process
         res.status(500).json(err);
